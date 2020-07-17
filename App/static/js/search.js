9fa3bcefb7788c52bc0d3d819b1da219b3f97624
Keshav Mishra <mishrakeshav@users.noreply.github.com>
2020-07-17 22:59:21 +0530

    Final changes

--- App/static/js/search.js
+++ App/static/js/search.js
@@ -10,9 +10,10 @@ searchBox.forEach(elm => {
 function myFunctionReq() {
     // Declare variables
     var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("myInput");
+    input = document.getElementById("reqstock_input");
     filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
+    console.log(filter)
+    table = document.getElementById("reqstock_table");
     tr = table.getElementsByTagName("tr");
 
     // Loop through all table rows, and hide those who don't match the search query
@@ -20,7 +21,8 @@ function myFunctionReq() {
     for (i = 0; i < tr.length; i++) {
 
         // Logic for 1st column
-        td = tr[i].getElementsByTagName("td")[0];
+        td = tr[i].getElementsByTagName("td")[1];
+        console.log()
         if (td) {
             txtValue = td.textContent || td.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -48,6 +50,64 @@ function myFunction() {
 
         // Logic for 1st column
         td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+       
+        
+    }
+    
+}
+
+function search_requests() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("request_input");
+    filter = input.value.toUpperCase();
+    
+    table = document.getElementById("request_table");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    
+    for (i = 0; i < tr.length; i++) {
+
+        // Logic for 1st column
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+       
+        
+    }
+    
+}
+
+function search_summary() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("summary_input");
+    filter = input.value.toUpperCase();
+    console.log(filter)
+    table = document.getElementById("summary_table");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    
+    for (i = 0; i < tr.length; i++) {
+
+        // Logic for 1st column
+        td = tr[i].getElementsByTagName("td")[2];
         if (td) {
             txtValue = td.textContent || td.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
