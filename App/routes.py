d28919d6164270b75f0f2efe0e1ec9f57b1ab14a
Keshav Mishra <mishrakeshav@users.noreply.github.com>
2020-07-24 13:38:16 +0530

    update navbar and loginpage

--- App/routes.py
+++ App/routes.py
@@ -367,3 +367,15 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+
+
+############# utils ####################
+def save_picture(form_picture, folder):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path , 'static/assets/' + folder , picture_fn)
+    i = Image.open(form_picture)
+    i.save(picture_path)
+    return picture_fn
